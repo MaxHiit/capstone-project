b4a2b2802a300a7820c0067975b7ebc47b7bbf0d
MaxHiit <maxime.baronce@hotmail.fr>
2021-04-07 16:56:25 +0200

    fix(sass source map): remove sass source map in webpack style rule

--- bundler/webpack.config.js
+++ bundler/webpack.config.js
@@ -5,13 +5,13 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   // webpack start building the bundle from this file
   entry: {
-    path: path.resolve(__dirname, '../src/index.js')
+    path: path.resolve(__dirname, '../src/index.js'),
   },
 
   // Webpack output name folder and bundle name
   output: {
     path: path.resolve(__dirname, '../dist'),
-    filename: 'index.bundle.js'
+    filename: 'index.bundle.js',
   },
 
   plugins: [
@@ -21,8 +21,8 @@ module.exports = {
     // Generates an HTML file from a template
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../public/index.html'),
-      favicon: path.resolve(__dirname, '../public/favicon.ico')
-    })
+      favicon: path.resolve(__dirname, '../public/favicon.ico'),
+    }),
   ],
 
   module: {
@@ -32,40 +32,33 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
-        type: 'javascript/auto'
+        type: 'javascript/auto',
       },
       {
         // Images: use asset/ressource module to copy images and put in static/images folder when you build
         test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
         type: 'asset/resource',
         generator: {
-          filename: './static/images/[fullhash][ext][query]'
-        }
+          filename: './static/images/[fullhash][ext][query]',
+        },
       },
       {
         // Fonts: use asset/ressource module to copy images and put in static/fonts folder when you build
         test: /\.(woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource',
         generator: {
-          filename: './static/fonts/[fullhash][ext][query]'
-        }
+          filename: './static/fonts/[fullhash][ext][query]',
+        },
       },
       {
         // Style: inject CSS into the head with source maps
         test: /\.(scss|sass|css)$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: { sourceMap: true, importLoaders: 1, modules: true }
-          },
-          { loader: 'sass-loader', options: { sourceMap: true } }
-        ]
-      }
-    ]
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
   },
   // You can choose not to add file extensions when importing.
   resolve: {
-    extensions: ['.js', '.jsx', '.json']
-  }
+    extensions: ['.js', '.jsx', '.json'],
+  },
 };
