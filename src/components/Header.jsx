128196ab4471275583afdd5b89cc7968d2750663
MaxHiit <maxime.baronce@hotmail.fr>
2021-04-08 16:07:11 +0200

    feat(count icon): adding count state close to cart icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Context } from '../Context';
 
 import Icon from './icons/Icon';
 
 const Header = () => {
+  const { countItem } = useContext(Context);
+
   return (
     <>
       <header>
@@ -12,7 +15,16 @@ const Header = () => {
             <h1>Pic Some</h1>
           </Link>
           <Link to='/carts'>
-            <Icon name='shop' width='30' height='30' color='#fff' />
+            <Icon name='shop' width='30' height='30' color='#fff' className='count-icon' />
+            <div
+              className={
+                countItem === 0
+                  ? 'count-wrapper count-wrapper--hidden'
+                  : 'count-wrapper count-wrapper--visible'
+              }
+            >
+              <span className='count'>{countItem}</span>
+            </div>
           </Link>
         </div>
       </header>
