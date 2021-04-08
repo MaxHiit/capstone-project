1a73e9d6d3a48cd9329a08f52cb2107b9493972f
MaxHiit <maxime.baronce@hotmail.fr>
2021-04-08 02:20:49 +0200

    feat(icon component): add reusable icon component with props

---
+++ src/components/icons/Icon.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import iconPath from './iconList';
+
+const Icon = ({ name, width, height, color, className = '', ...rest }) => {
+  return (
+    <div className={className} {...rest}>
+      <svg
+        viewBox='0 0 24 24'
+        width={`${width}px`}
+        height={`${height}px`}
+        xmlns='http://www.w3.org/2000/svg'
+        xmlnsXlink='http://www.w3.org/1999/xlink'
+      >
+        <path fill={color} d={iconPath[name]} />
+      </svg>
+    </div>
+  );
+};
+
+export default Icon;
