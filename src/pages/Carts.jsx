53afdfc6029218e0873a2a1f65f79d9ee7c27d97
MaxHiit <maxime.baronce@hotmail.fr>
2021-04-08 02:16:11 +0200

    feat(cart page): creation of the page

---
+++ src/pages/Carts.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Carts = () => {
+  return (
+    <main className='container'>
+      <div className='cart-page'>
+        <h1>Cart Pages</h1>
+      </div>
+    </main>
+  );
+};
+
+export default Carts;
