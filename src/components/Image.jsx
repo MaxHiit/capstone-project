90264b06ed1430c1f366456aeab6c6bea19e53a7
MaxHiit <maxime.baronce@hotmail.fr>
2021-04-08 02:31:54 +0200

    feat(image component):
    creation of the image component structure, the like feature and hover
    action

---
+++ src/components/Image.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Context } from '../Context';
+
+import Icon from './icons/Icon';
+
+const Image = ({ img, className }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const { toggleFavorite, addToCart } = useContext(Context);
+
+  const playHoverFunc = () => {
+    setIsHovered(true);
+  };
+
+  const stopHoverFunc = () => {
+    setIsHovered(false);
+  };
+
+  // const getF
+
+  return (
+    <div
+      className={
+        img.isFavorite
+          ? `image-container image--active ${className}`
+          : `image-container ${className}`
+      }
+      onMouseEnter={playHoverFunc}
+      onMouseLeave={stopHoverFunc}
+    >
+      <img src={img.url} alt='' className='image-grid' />
+      <div className='image_icon-container'>
+        {isHovered && (
+          <>
+            <Icon
+              name='heart'
+              width='30'
+              height='30'
+              color='#fff'
+              className={!img.isFavorite ? 'image_icon' : 'image_icon image_icon--active'}
+              onClick={() => {
+                toggleFavorite(img.id);
+              }}
+            />
+            <Icon
+              name='plus'
+              width='30'
+              height='30'
+              color='#fff'
+              className='image_icon'
+              onClick={() => addToCart(img)}
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+Image.propTypes = {
+  className: PropTypes.string,
+  img: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    isFavorite: PropTypes.bool,
+  }),
+};
+
+export default Image;
