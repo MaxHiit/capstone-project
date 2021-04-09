c9c7b4058b7213c1108c923a8996558cfc04f5f2
MaxHiit <maxime.baronce@hotmail.fr>
2021-04-09 21:31:23 +0200

    feat(hover custom hook): adding hover custom hook logic

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,20 +1,70 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Context } from '../Context';
+import useHover from '../hooks/useHover';
 
 import Icon from './icons/Icon';
 
 const Image = ({ img, className }) => {
-  const [isHovered, setIsHovered] = useState(false);
-  const { toggleFavorite, addToCart } = useContext(Context);
+  const [isHovered, ref] = useHover();
+  const { toggleFavorite, cartItems, addToCart, removeFromCart } = useContext(Context);
 
-  const playHoverFunc = () => {
-    setIsHovered(true);
-  };
+  function heartIcon() {
+    if (img.isFavorite) {
+      return (
+        <Icon
+          name='heart'
+          width='30'
+          height='30'
+          color='#fff'
+          className='image_icon image_icon--heart image--active'
+          onClick={() => {
+            toggleFavorite(img.id);
+          }}
+        />
+      );
+    } else if (isHovered) {
+      return (
+        <Icon
+          name='heart'
+          width='30'
+          height='30'
+          color='#fff'
+          className='image_icon image_icon--heart'
+          onClick={() => {
+            toggleFavorite(img.id);
+          }}
+        />
+      );
+    }
+  }
 
-  const stopHoverFunc = () => {
-    setIsHovered(false);
-  };
+  function cartIcon() {
+    const alreadyInCart = cartItems.some((item) => item.id === img.id);
+    if (alreadyInCart) {
+      return (
+        <Icon
+          name='plus'
+          width='30'
+          height='30'
+          color='#fff'
+          className='image_icon image_icon--plus image_icon--added'
+          onClick={() => removeFromCart(img.id)}
+        />
+      );
+    } else if (isHovered) {
+      return (
+        <Icon
+          name='plus'
+          width='30'
+          height='30'
+          color='#fff'
+          className='image_icon image_icon--plus'
+          onClick={() => addToCart(img)}
+        />
+      );
+    }
+  }
 
   return (
     <div
@@ -23,33 +73,12 @@ const Image = ({ img, className }) => {
           ? `image-container image--active ${className}`
           : `image-container ${className}`
       }
-      onMouseEnter={playHoverFunc}
-      onMouseLeave={stopHoverFunc}
+      ref={ref}
     >
-      <img src={img.url} alt='' className='image-grid' />
+      <img src={img.url} alt='' className='image-grid' loading='lazy' />
       <div className='image_icon-container'>
-        {isHovered && (
-          <>
-            <Icon
-              name='heart'
-              width='30'
-              height='30'
-              color='#fff'
-              className={!img.isFavorite ? 'image_icon' : 'image_icon image_icon--active'}
-              onClick={() => {
-                toggleFavorite(img.id);
-              }}
-            />
-            <Icon
-              name='plus'
-              width='30'
-              height='30'
-              color='#fff'
-              className='image_icon'
-              onClick={() => addToCart(img)}
-            />
-          </>
-        )}
+        {heartIcon()}
+        {cartIcon()}
       </div>
     </div>
   );
